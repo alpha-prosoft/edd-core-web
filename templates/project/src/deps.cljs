e257fffcd98db244d7bcb288b4e089f80e658719
ewp10022 <robert.pofuk@rbinternational.com>
2024-01-17 19:58:40 +0100

    [AP-489] Update deps
    Change-Id: I8886c968f0f11a8b9a7a1425c60f793f9789c030

--- templates/project/src/deps.cljs
+++ templates/project/src/deps.cljs
@@ -1,7 +1,8 @@
-{:npm-deps {"shadow-cljs"         "2.20.20"
-            "@mui/material"       "5.11.7"
-            "@mui/icons-material" "5.11.0"
+{:npm-deps {"shadow-cljs"         "2.26.5"
+            "@mui/material"       "5.15.3"
+            "@mui/icons-material" "5.15.3"
+            "@emotion/react"      "11.11.3"
+            "@emotion/styled"     "11.11.0"
             "react"               "18.2.0"
-            "react-dom"           "18.2.0"
-            "@emotion/styled"     "11.10.5"
-            "@emotion/react"      "11.10.5"}}
+            "react-dom"           "18.2.0"}}
+
